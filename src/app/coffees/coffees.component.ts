701933c289027cac34dbc8a81c122e72d519b8ac
afDonoso <andy.donoso1@gmail.com>
2024-11-01 15:07:41 -0500

    Add coffee type count

--- src/app/coffees/coffees.component.ts
+++ src/app/coffees/coffees.component.ts
@@ -9,6 +9,8 @@ import { Coffee } from '../../shared/models/coffee';
 })
 export class CoffeesComponent implements OnInit {
   coffees: Coffee[] = [];
+  originTypeCount = 0;
+  blendTypeCount = 0;
 
   constructor(private readonly coffeeService: CoffeeService) { }
 
@@ -17,150 +19,10 @@ export class CoffeesComponent implements OnInit {
   }
 
   getCoffees() {
-    this.coffeeService.getCoffees().subscribe(coffees => this.coffees = [...coffees, {
-      "id": 6,
-      "nombre": "Café Especial La Manchuria",
-      "tipo": "Blend",
-      "region": "Salgar, Antioquia",
-      "sabor": "Vino, Notas dulces, Frutos rojos",
-      "altura": 1850,
-      "imagen": "https://cdn.shopify.com/s/files/1/0272/2873/3504/products/cafe-especal-manchuria-cafe-colombiano-f_720x.png"
-    }, {
-      "id": 6,
-      "nombre": "Café Especial La Manchuria",
-      "tipo": "Blend",
-      "region": "Salgar, Antioquia",
-      "sabor": "Vino, Notas dulces, Frutos rojos",
-      "altura": 1850,
-      "imagen": "https://cdn.shopify.com/s/files/1/0272/2873/3504/products/cafe-especal-manchuria-cafe-colombiano-f_720x.png"
-    }, {
-      "id": 6,
-      "nombre": "Café Especial La Manchuria",
-      "tipo": "Blend",
-      "region": "Salgar, Antioquia",
-      "sabor": "Vino, Notas dulces, Frutos rojos",
-      "altura": 1850,
-      "imagen": "https://cdn.shopify.com/s/files/1/0272/2873/3504/products/cafe-especal-manchuria-cafe-colombiano-f_720x.png"
-    }, {
-      "id": 6,
-      "nombre": "Café Especial La Manchuria",
-      "tipo": "Blend",
-      "region": "Salgar, Antioquia",
-      "sabor": "Vino, Notas dulces, Frutos rojos",
-      "altura": 1850,
-      "imagen": "https://cdn.shopify.com/s/files/1/0272/2873/3504/products/cafe-especal-manchuria-cafe-colombiano-f_720x.png"
-    }, {
-      "id": 6,
-      "nombre": "Café Especial La Manchuria",
-      "tipo": "Blend",
-      "region": "Salgar, Antioquia",
-      "sabor": "Vino, Notas dulces, Frutos rojos",
-      "altura": 1850,
-      "imagen": "https://cdn.shopify.com/s/files/1/0272/2873/3504/products/cafe-especal-manchuria-cafe-colombiano-f_720x.png"
-    }, {
-      "id": 6,
-      "nombre": "Café Especial La Manchuria",
-      "tipo": "Blend",
-      "region": "Salgar, Antioquia",
-      "sabor": "Vino, Notas dulces, Frutos rojos",
-      "altura": 1850,
-      "imagen": "https://cdn.shopify.com/s/files/1/0272/2873/3504/products/cafe-especal-manchuria-cafe-colombiano-f_720x.png"
-    }, {
-      "id": 6,
-      "nombre": "Café Especial La Manchuria",
-      "tipo": "Blend",
-      "region": "Salgar, Antioquia",
-      "sabor": "Vino, Notas dulces, Frutos rojos",
-      "altura": 1850,
-      "imagen": "https://cdn.shopify.com/s/files/1/0272/2873/3504/products/cafe-especal-manchuria-cafe-colombiano-f_720x.png"
-    }, {
-      "id": 6,
-      "nombre": "Café Especial La Manchuria",
-      "tipo": "Blend",
-      "region": "Salgar, Antioquia",
-      "sabor": "Vino, Notas dulces, Frutos rojos",
-      "altura": 1850,
-      "imagen": "https://cdn.shopify.com/s/files/1/0272/2873/3504/products/cafe-especal-manchuria-cafe-colombiano-f_720x.png"
-    }, {
-      "id": 6,
-      "nombre": "Café Especial La Manchuria",
-      "tipo": "Blend",
-      "region": "Salgar, Antioquia",
-      "sabor": "Vino, Notas dulces, Frutos rojos",
-      "altura": 1850,
-      "imagen": "https://cdn.shopify.com/s/files/1/0272/2873/3504/products/cafe-especal-manchuria-cafe-colombiano-f_720x.png"
-    }, {
-      "id": 6,
-      "nombre": "Café Especial La Manchuria",
-      "tipo": "Blend",
-      "region": "Salgar, Antioquia",
-      "sabor": "Vino, Notas dulces, Frutos rojos",
-      "altura": 1850,
-      "imagen": "https://cdn.shopify.com/s/files/1/0272/2873/3504/products/cafe-especal-manchuria-cafe-colombiano-f_720x.png"
-    }, {
-      "id": 6,
-      "nombre": "Café Especial La Manchuria",
-      "tipo": "Blend",
-      "region": "Salgar, Antioquia",
-      "sabor": "Vino, Notas dulces, Frutos rojos",
-      "altura": 1850,
-      "imagen": "https://cdn.shopify.com/s/files/1/0272/2873/3504/products/cafe-especal-manchuria-cafe-colombiano-f_720x.png"
-    }, {
-      "id": 6,
-      "nombre": "Café Especial La Manchuria",
-      "tipo": "Blend",
-      "region": "Salgar, Antioquia",
-      "sabor": "Vino, Notas dulces, Frutos rojos",
-      "altura": 1850,
-      "imagen": "https://cdn.shopify.com/s/files/1/0272/2873/3504/products/cafe-especal-manchuria-cafe-colombiano-f_720x.png"
-    }, {
-      "id": 6,
-      "nombre": "Café Especial La Manchuria",
-      "tipo": "Blend",
-      "region": "Salgar, Antioquia",
-      "sabor": "Vino, Notas dulces, Frutos rojos",
-      "altura": 1850,
-      "imagen": "https://cdn.shopify.com/s/files/1/0272/2873/3504/products/cafe-especal-manchuria-cafe-colombiano-f_720x.png"
-    }, {
-      "id": 6,
-      "nombre": "Café Especial La Manchuria",
-      "tipo": "Blend",
-      "region": "Salgar, Antioquia",
-      "sabor": "Vino, Notas dulces, Frutos rojos",
-      "altura": 1850,
-      "imagen": "https://cdn.shopify.com/s/files/1/0272/2873/3504/products/cafe-especal-manchuria-cafe-colombiano-f_720x.png"
-    }, {
-      "id": 6,
-      "nombre": "Café Especial La Manchuria",
-      "tipo": "Blend",
-      "region": "Salgar, Antioquia",
-      "sabor": "Vino, Notas dulces, Frutos rojos",
-      "altura": 1850,
-      "imagen": "https://cdn.shopify.com/s/files/1/0272/2873/3504/products/cafe-especal-manchuria-cafe-colombiano-f_720x.png"
-    }, {
-      "id": 6,
-      "nombre": "Café Especial La Manchuria",
-      "tipo": "Blend",
-      "region": "Salgar, Antioquia",
-      "sabor": "Vino, Notas dulces, Frutos rojos",
-      "altura": 1850,
-      "imagen": "https://cdn.shopify.com/s/files/1/0272/2873/3504/products/cafe-especal-manchuria-cafe-colombiano-f_720x.png"
-    }, {
-      "id": 6,
-      "nombre": "Café Especial La Manchuria",
-      "tipo": "Blend",
-      "region": "Salgar, Antioquia",
-      "sabor": "Vino, Notas dulces, Frutos rojos",
-      "altura": 1850,
-      "imagen": "https://cdn.shopify.com/s/files/1/0272/2873/3504/products/cafe-especal-manchuria-cafe-colombiano-f_720x.png"
-    }, {
-      "id": 6,
-      "nombre": "Café Especial La Manchuria",
-      "tipo": "Blend",
-      "region": "Salgar, Antioquia",
-      "sabor": "Vino, Notas dulces, Frutos rojos",
-      "altura": 1850,
-      "imagen": "https://cdn.shopify.com/s/files/1/0272/2873/3504/products/cafe-especal-manchuria-cafe-colombiano-f_720x.png"
-    },]);
+    this.coffeeService.getCoffees().subscribe(coffees => {
+      this.coffees = coffees;
+      this.originTypeCount = coffees.filter(c => c.tipo === 'Café de Origen').length;
+      this.blendTypeCount = coffees.filter(c => c.tipo === 'Blend').length;
+    });
   }
 }
